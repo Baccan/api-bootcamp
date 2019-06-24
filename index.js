74145d7b088ec01603756c60290cdf35df9511e2
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-24 16:34:51 -0300

    Rota GET para teste com array

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const server = express();
 const port = 3000;
 
+const users = ["Baccan", "Diego", "Flávio"];
+
 // Query params = ?teste=1
 server.get("/teste", (req, res) => {
   const nome = req.query.nome;
@@ -10,10 +12,10 @@ server.get("/teste", (req, res) => {
 });
 
 // Route params = /useres/1
-server.get("/users/:id", (req, res) => {
-  const { id } = req.params;
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
 
-  res.json({ message: `Buscando usuário ${id}!` });
+  res.json(users[index]);
 });
 
 // Request body = { "name": "Baccan", "email": "gustavobaccang@gmail.com" }
