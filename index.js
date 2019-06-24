68f96716af3bedb4535049962b8b9a8543e97654
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-24 16:23:49 -0300

    Query & Route params

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require("express");
 const server = express();
 const port = 3000;
 
-server.get("/", (req, res) => res.json({ message: "Hello World!" }));
+// Query params = ?teste=1
+server.get("/teste", (req, res) => {
+  const nome = req.query.nome;
+
+  res.json({ message: `Hello ${nome}!` });
+});
+
+// Route params = /useres/1
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({ message: `Buscando usuário ${id}!` });
+});
+
+// Request body = { "name": "Baccan", "email": "gustavobaccang@gmail.com" }
 
 server.listen(port, () =>
   console.log(`Example server listening on port ${port}!`)
