9fdba1619b02f63a45c55cb499ed2e640de747d9
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-24 17:08:41 -0300

    Middleware global

--- index.js
+++ index.js
@@ -7,25 +7,31 @@ const port = 3000;
 
 const users = ["Baccan", "Diego", "Flávio"];
 
+// Middleware global
+server.use((req, res, next) => {
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+  return next();
+});
+
 // CRUD - Create, Read, Update, Delete
 
 // Query params = ?teste=1
 server.get("/teste", (req, res) => {
   const nome = req.query.nome;
 
-  res.json({ message: `Hello ${nome}!` });
+  return res.json({ message: `Hello ${nome}!` });
 });
 
 // Rota sem params
 server.get("/users", (req, res) => {
-  res.json(users);
+  return res.json(users);
 });
 
 // Route params = /useres/1
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
 
-  res.json(users[index]);
+  return res.json(users[index]);
 });
 
 // Request body = { "name": "Baccan", "email": "gustavobaccang@gmail.com" }
