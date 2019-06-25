06920be64a422ad6a2977f0ddfc5f2e84926a3b0
Baccan <gustavobaccang@gmail.com>
2019-06-25 00:51:18 +0000

    Middlewares globais e locais

--- index.js
+++ index.js
@@ -9,9 +9,33 @@ const users = ["Baccan", "Diego", "Flávio"];
 
 // Middleware global
 server.use((req, res, next) => {
+  console.time("Request");
+
   console.log(`Método: ${req.method}; URL: ${req.url}`);
-  return next();
+  next();
+  console.timeEnd("Request");
 });
+
+// Middleware local
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: "User does not exists" });
+  }
+
+  req.user = user;
+
+  return next();
+}
 
 // CRUD - Create, Read, Update, Delete
 
@@ -28,14 +52,15 @@ server.get("/users", (req, res) => {
 });
 
 // Route params = /useres/1
-server.get("/users/:index", (req, res) => {
-  const { index } = req.params;
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  // const { index } = req.params;
 
-  return res.json(users[index]);
+  // return res.json(users[index]);
+  return res.json(req.user);
 });
 
 // Request body = { "name": "Baccan", "email": "gustavobaccang@gmail.com" }
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -43,7 +68,7 @@ server.post("/users", (req, res) => {
   return res.json(users);
 });
 
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserExists, checkUserInArray, (req, res) => {
   const { name } = req.body;
   const { index } = req.params;
 
@@ -52,7 +77,7 @@ server.put("/users/:index", (req, res) => {
   return res.json(users);
 });
 
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
